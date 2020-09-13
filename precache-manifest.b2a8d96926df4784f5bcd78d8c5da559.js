abf0c3938c4f716968e5ba9e9904c7095c0d6793
강재현 <kangjaehyeon@kangjaehyeon-ui-MacBook-Pro.local>
2020-09-13 19:19:16 +0900

    Updates

---
+++ precache-manifest.b2a8d96926df4784f5bcd78d8c5da559.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ea36c0f33d633ab7f814e5dfbbe1356",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "507d70e15ca75fe5e410",
+    "url": "/movie_app/static/css/main.2a757360.chunk.css"
+  },
+  {
+    "revision": "2cb00f0b337cb1b1485d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "507d70e15ca75fe5e410",
+    "url": "/movie_app/static/js/main.3c12ef8e.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
